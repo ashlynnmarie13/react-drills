eb22e70bb748ab0b3d5023cdc4e9b5e631617afd
user1 <user1@user1s-mbp.attlocal.net>
2018-06-06 23:19:56 -0500

    second commit

---
+++ app-7/src/List.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Todo from "./Todo";
+
+function List(props) {
+  let list = props.tasks.map((element, index) => {
+    return <Todo key={index} task={element} />;
+  });
+  return <div>{list}</div>;
+}
+
+export default List;
---
+++ app-7/src/Todo.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+function Todo(props) {
+  return <p>{props.task}</p>;
+}
+
+export default Todo;
