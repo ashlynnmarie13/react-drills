a914b81ccccb3c17111da4cc66c558951a0db793
user1 <user1@user1s-mbp.attlocal.net>
2018-06-06 23:26:57 -0500

    third commit, fixed error

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -4,24 +4,34 @@ import Todo from "./Todo";
 class App extends Component {
   constructor() {
     super();
-
+    //our data that we'll be tracking is a list, an empty array
     this.state = {
       list: []
     };
+    // we're binding the handleAdd function to make it work in render
+    this.handleTask = this.handleTask.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
   }
-
+  //we're passing in the value to handle task. This sets the state of the input
+  // to the value passed in
   handleTask(value) {
-    this.setState = { input: value };
+    this.setState({ input: value });
   }
-
+  // this method sets the state of the list to the state of the OLD list AND
+  // the state of the NEW list... input starts as an empty string
   handleAdd() {
     this.setState({
       list: [...this.state.list, this.state.input],
       input: ""
     });
   }
-
+  // new variable is list- we're mapping over the current state of the list array
+  // returning what Todo through a child component
+  // in order to render multiple components in the same structure,
+  // you have to use keys to differentiate them
+  // huge list, each Todo is going to be a li item
+  // if we don't have index then card analogy, each key is an index
+  // the task prop is the actual value, which is the entire element
   render() {
     let list = this.state.list.map((element, index) => {
       return <Todo key={index} task={element} />;
